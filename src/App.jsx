310765bf4ce4b6b4ae095f83f772a2de69ab3630
Pedro Lima <pedrlimadev@gmail.com>
2025-05-13 11:07:49 -0300

    project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [responseMessage, setResponseMessage] = useState("Me pergunte algo!");
+  const { register, handleSubmit } = useForm();
+
+  const handleFormSubmit = async ({ prompt }) => {};
+
+  return (
+    <section className="section-settings">
+      <div className="chat-box">
+        <p className="p-settings">{responseMessage}</p>
+      </div>
+
+      <form className="ask-box" onSubmit={handleSubmit(handleFormSubmit)}>
+        <label>
+          <input
+            className="form-style"
+            {...register("prompt")}
+            type="text"
+            placeholder="Pergunte algo para o BookStoreChatbot"
+          />
+        </label>
+        <button type="submit" className="button-style">
+          Enviar
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
